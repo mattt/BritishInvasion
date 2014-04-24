726002c2eaffea82d37b129476b2c11583143228
Mattt Thompson <m@mattt.me>
2014-04-23 22:54:31 -0700

    Update BritishInvasion.m

--- BritishInvasion/BritishInvasion.m
+++ BritishInvasion/BritishInvasion.m
@@ -1,6 +1,6 @@
 // BritishInvasion.m
 // 
-// Copyright (c) 2014年 Mattt Thompson
+// Copyright (c) 2014 Mattt Thompson
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
